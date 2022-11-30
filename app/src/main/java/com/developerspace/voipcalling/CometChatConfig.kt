9b65ec3f84f8f0e6a887b396a58fe93c82a4ab0e
teo <teo@DESKTOP-08UKAEE>
2022-11-30 19:11:25 +0200

    initial commit

---
+++ app/src/main/java/com/developerspace/voipcalling/CometChatConfig.kt
@@ -0,0 +1,9 @@
+package com.developerspace.voipcalling
+
+class CometChatConfig {
+    companion object {
+        var APP_ID = "2267903ac12c0441"     //Replace with CometChat APP ID
+        var KEY = "0cd87e6ae4bc46e33576ba09b775b87c431e5604" //Replace with CometChat Key
+        var REGION = "eu" //Replace with CometChat App Region
+    }
+}
